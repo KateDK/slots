3c9b4e136904ca3919ff939457979ce511201709
Kate Dubitski <kate.dubitski@gmail.com>
2020-05-06 17:53:40 -0400

    Add slot logic

--- App.js
+++ App.js
@@ -1,3 +1,12 @@
+const symbols = ["X","O","7"];
+
+const playRound = ()=>{
+  const res = [];
+  for(let i = 0; i < 3; i++){
+     res.push(symbols[Math.floor(Math.random()*3)]);
+  }
+  return res;
+};
 class App extends React.Component{
   render(){
    return <h1>Hello World</h1>;
