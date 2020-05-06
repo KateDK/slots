11955fde979b9a87114cd40a6cb935fc27398fe8
Kate Dubitski <kate.dubitski@gmail.com>
2020-05-06 19:13:43 -0400

    Refactor row rendering to use map
    -add classname to span holding symbol

--- Row.js
+++ Row.js
@@ -1,12 +1,10 @@
 const Row = (props)=>{
   const gameRow = props.row();
-  const row = gameRow.join("");
   return(
     <div>
-      {/* <div>
-        {gameRow.map(elm=> <span key={Math.random()}>{elm}</span>)}
-      </div> */}
-      <span>{row}</span>
+      <div>
+        {gameRow.map(elm=> <span key={Math.random()} className="symbol">{elm}</span>)}
+      </div>
       <Msg win={gameRow[0]===gameRow[1] && gameRow[1] === gameRow[2]}/>
     </div>
   );
