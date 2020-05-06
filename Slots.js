0f4bcb0506beff266c892105c29d97eb79b9a16f
Kate Dubitski <kate.dubitski@gmail.com>
2020-05-06 17:56:05 -0400

    Create Row component
    -win calculation passed to row

--- Slots.js
+++ Slots.js
@@ -7,3 +7,13 @@ const Slots = (props)=>{
 
   );
 };
+const Row = (props)=>{
+  const gameRow = props.row();
+  const row = gameRow.join("");
+  return(
+    <div>
+      <span>{row}</span>
+      <Msg win={gameRow[0]===gameRow[1] && gameRow[1] === gameRow[2]}/>
+    </div>
+  );
+};
